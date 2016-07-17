50b2a3de6bbaa3ecd632bc8957aa07dbbacca13f
angus-c <anguscroll@gmail.com>
2016-07-17 14:28:57 -0700

    remove comparator, add optimization flags

--- packages/array-unique/index.js
+++ packages/array-unique/index.js
@@ -6,27 +6,38 @@ module.exports = unique;
   var b = {b: 4};
   var c = {c: 5};
   unique([a, a, b, c, b]); // [a, b, c]
-  unique([1, '1', 2, '2', 3, 2]); // [1, 2, 3]
-  unique([1, '1', 2, '2', 3, 2], fuction(a, b) {return a === b}; // [1, '1', 2, '2', 3]
+  unique([1, '1', 2, '2', 3, 2]); // [1, '1', 2, '2', 3]
+  // declaring sorted array for performance
+  unique([1, 1, '1', 2, 2, 5, '5', '5'], true); // [1, '1', 2, 5, '6']
+  // declaring strings array for performance
+  unique(['a', 'c', 'b', 'c', 'a'], false, true); // ['a', 'b', 'c']
 */
 
-function unique(arr, comparator) {
-  var result = [];
+function unique(arr, sorted, strings) {
+  if (!sorted && strings && (arr[0] !== Object(arr[0]))) {
+    return stringUnique(arr);
+  }
+  var result = [], duplicate, lastAdded;
   var len = arr.length;
   for (var i = 0; i < len; i++) {
     var elem = arr[i];
-    var duplicate = false;
-    if (comparator) {
-      for (var j = 0; j < result.length; j++) {
-        if (comparator(elem, result[j])) {
-          duplicate = true;
-          break;
-        }
-      }
-    } else {
+    duplicate = lastAdded && (lastAdded === elem);
+    if (!duplicate && !sorted) {
       duplicate = result.indexOf(elem) > -1;
     }
-    duplicate || result.push(elem);
+    if (!duplicate) {
+      result.push(elem);
+      lastAdded = elem;
+    }
   }
   return result;
+}
+
+function stringUnique(arr) {
+  var lookup = {};
+  var len = arr.length;
+  for (var i = 0; i < len; i++) {
+    lookup[arr[i]] = true;
+  }
+  return Object.keys(lookup);
 }
--- test/array-unique/index.js
+++ test/array-unique/index.js
@@ -8,6 +8,21 @@ test('primitives', function (t) {
   t.deepEqual(unique([true, true, false, false, true]), [true, false]);
 });
 
+// test('perf', function (t) {
+//   var arr = [];
+//   var size = 1000000;
+//   var i = size;
+//   console.time('uniq');
+//   while (i--) {
+//     arr.push(String(i)); arr.push(String(i));
+//   }
+//   var arr1 = unique(arr, true, true);
+//   console.timeEnd('uniq');
+//   // console.log(arr1);
+//   t.ok(arr1.length == size);
+//   t.end();
+// });
+
 test('objects by equality', function (t) {
   t.plan(3);
   var a = {a: 1};
@@ -24,21 +39,41 @@ test('objects by equality', function (t) {
   t.deepEqual(unique([arr1, arr3, arr3, arr1, arr2]), [arr1, arr3, arr2]);
 });
 
-test('objects by comparator', function (t) {
-  t.plan(2);
-  var arr1 = [{x: 1}, {x: 2}, {x: 1}, {x: 3}, {x: 2}, {x: 1}, {x: 2}];
-  var comparator = function (a, b) {return a.x === b.x;};
-  t.deepEqual(unique(arr1, comparator), [{x: 1}, {x: 2}, {x: 3}]);
-  var arr2 = [
-    function () {},
-    function (a) {},
-    function () {},
-    function (a, b) {},
-    function () {}
-  ];
-  var comparator2 = function (a, b) {return a.length === b.length;};
-  t.deepEqual(
-    unique(arr2, comparator2).map(function (fn) {return fn.length;}),
-    [0, 1, 2]
-  );
+test('presorted arrays', function (t) {
+  t.plan(5);
+  t.deepEqual(unique([1, 1, 2, 2, 3, 3, 4, 4], true), [1, 2, 3, 4]);
+  t.deepEqual(unique(['a', 'a', 'b', 'b', 'c', 'c'], true), ['a', 'b', 'c']);
+  var a = {a: 1};
+  var b = {b: 3};
+  var c = {c: 2};
+  t.deepEqual(unique([a, a, b, b, c, c], true), [a, b, c]);
+  // bogus sorted declarations fail
+  t.notDeepEqual(unique([1, 2, 1, 3, 4, 4, 1], true), [1, 2, 3, 4]);
+  t.notDeepEqual(unique([a, b, a, c, b, b], true), [a, b, c]);
+});
+
+test('using strings option', function (t) {
+  t.plan(7);
+  // works for arrays of strings
+  t.deepEqual(unique(['a', 'b', 'a', 'b', 'a', 'c'], false, true), ['a', 'b', 'c']);
+
+  // converts other homogenous primitives to strings
+  t.deepEqual(unique([1, 2, 1, 2, 3, 3, 4, 4], false, true), ['1', '2', '3', '4']);
+  t.deepEqual(unique([false, true, false, false], false, true), ['false', 'true']);
+
+  // works for arrays of same-type non-primitives (ignores strings flag)
+  var a = {a: 1};
+  var b = {b: 3};
+  var c = {c: 2};
+  t.deepEqual(unique([a, b, a, b, b, c], false, true), [a, b, c]);
+  var fn1 = function () {};
+  var fn2 = function () {};
+  var fn3 = function () {};
+  t.deepEqual(unique([fn1, fn2, fn3, fn2, fn3, fn2], false, true), [fn1, fn2, fn3]);
+
+  // fails for arrays of different-type primitives
+  t.notDeepEqual(unique([1, 2, '1', 2, '3', false], false, true), [1, 2, '1', '3', false]);
+
+  // works when sorted option overrides
+  t.deepEqual(unique(['a', 'b', 'b', 'b', 'b', 'c'], true, true), ['a', 'b', 'c']);
 });
