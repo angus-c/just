916ffa2807abfc895e20cc1a0701366aa8dbf01c
Craig Beck <luckybonza@gmail.com>
2016-07-18 13:26:27 -0700

    remove global.___ in favor of undefined keyword

--- test/function-partial/index.js
+++ test/function-partial/index.js
@@ -1,11 +1,11 @@
 var test = require('tape');
 var partial = require('../../packages/function-partial');
 
-var _ = global.___;
+// var _ = global.___;
 
 test('binds to supplied arguments placeholders', function (t) {
   t.plan(2);
-  var cubeRoot = partial(Math.pow, _, 1 / 3);
+  var cubeRoot = partial(Math.pow, undefined, 1 / 3);
   t.equal(cubeRoot(10).toFixed(1), '2.2');
   t.equal(cubeRoot(35).toFixed(1), '3.3');
   t.end();
@@ -15,7 +15,7 @@ test('surplus supplied arguments ignored', function (t) {
   function max() {
     return Math.max.apply(null, arguments);
   }
-  var atLeast4 = partial(max, _, 4);
+  var atLeast4 = partial(max, undefined, 4);
   t.equal(atLeast4(7), 7);
   t.equal(atLeast4(3, 9), 4);
   t.end();
@@ -38,7 +38,7 @@ test('executes in the correct context', function (t) {
   function nthRoot(input, n) {
     return Math.pow(input, 1 / n).toFixed(this.dps);
   }
-  var fourthRoot = partial(nthRoot, _, 4);
+  var fourthRoot = partial(nthRoot, undefined, 4);
   t.equal(fourthRoot.call({dps: 0}, 4345), '8');
   t.equal(fourthRoot.call({dps: 2}, 4345), '8.12');
   t.end();
