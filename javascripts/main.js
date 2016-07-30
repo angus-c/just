c5accdeccced9ad2f02d03ab894ea3e25788bd80
angus-c <anguscroll@gmail.com>
2016-07-30 13:50:49 -0700

    auto generate menu

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log('type of ', typeof App);
-console.log('type of Class', typeof new class Hello {} );
-
 ReactDOM.render(<App/>, document.querySelector('#react-root'));
