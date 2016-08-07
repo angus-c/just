05580d26bec90b2dc85b5418ff90e978ba263f15
angus-c <anguscroll@gmail.com>
2016-08-06 20:24:30 -0700

    make it break differenly

--- javascripts/mainn.js
+++ javascripts/mainn.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-ReactDOM.render(<Appp/>, document.querySelector('#react-root'));
+ReactDOM.render(<App/>, document.querySelector('#react-root'));
