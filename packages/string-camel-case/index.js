d16b53bc9b3ca487da98fbcf9281c1b99338c99e
angus-c <anguscroll@gmail.com>
2016-12-30 19:09:46 -0800

    cp

---
+++ packages/string-camel-case/index.js
@@ -0,0 +1,29 @@
+module.exports = camelCase;
+
+/*
+  camelCase('the quick brown fox'); // 'theQuickBrownFox'
+  camelCase('the_quick_brown_fox'); // 'theQuickBrownFox'
+  camelCase('the-quick-brown-fox'); // 'theQuickBrownFox'
+  camelCase('theQuickBrownFox'); // 'theQuickBrownFox'
+  camelCase('the - quick * brown# fox'); // 'theQuickBrownFox'
+  camelCase('theQUICKBrownFox'); // 'theQUICKBrownFox'
+*/
+
+// any combination of spaces and punctuation characters
+// thanks to http://stackoverflow.com/a/25575009
+var wordSeparators = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/
+
+function camelCase(str) {
+  var words = str.split(wordSeparators);
+  var len = words.length;
+  var mappedWords = new Array(len);
+  for (var i=0; i < len; i++) {
+    var word = words[i];
+    var firstLetter = word[0];
+    if (i > 0) {
+      firstLetter = firstLetter.toUpperCase();
+    }
+    mappedWords[i] = firstLetter + word.slice(1);
+  }
+  return mappedWords.join('');
+}
