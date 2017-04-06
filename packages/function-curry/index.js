f9987e231cc248ebda2978db144520a9e3c196c3
chee <chee@snaek.org>
2017-04-06 12:47:43 +0100

    make curry translate polyadic to sequence of unary

--- packages/function-curry/index.js
+++ packages/function-curry/index.js
@@ -9,12 +9,20 @@ module.exports = curry;
   milesToKm(10); // 16.2
 */
 
-function curry(fn /*, arg1, arg2 etc */) {
-  var curriedArgs = [].slice.call(arguments, 1);
-  if (!curriedArgs.length) {
-    return fn;
+function curry(fn /* arg1, arg2 etc */) {
+  var fnArgs = [].slice.call(arguments, 1);
+  if (fnArgs.length >= fn.length) {
+    return fn.apply(this, fnArgs);
   }
-  return function () {
-    return fn.apply(this, curriedArgs.concat([].slice.call(arguments)));
+  return function currier() {
+    var curriedArgs = [].slice.call(arguments);
+    var args = fnArgs.concat(curriedArgs);
+    if (args.length >= fn.length) {
+      return fn.apply(this, args);
+    } else {
+      return function () {
+        return currier.apply(this, curriedArgs.concat([].slice.call(arguments)));
+      };
+    }
   };
 }
