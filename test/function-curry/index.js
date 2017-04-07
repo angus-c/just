810441a39d82563eeaee0a812b226dd0dcec9b1a
chee <chee@snaek.org>
2017-04-07 08:14:57 +0100

    add test for not allowing to pass initial args
    fix other tests for this change

--- test/function-curry/index.js
+++ test/function-curry/index.js
@@ -1,26 +1,26 @@
 var test = require('tape');
 var curry = require('../../packages/function-curry');
 
-test('binds curried arguments to supplied arguments', function (t) {
+test('returns a curried function', function (t) {
+  t.plan(1);
+  function converter(ratio, input) {
+    return (input * ratio).toFixed(1);
+  }
+  var curriedConverter = curry(converter);
+  var milesToKm = curriedConverter(1.62);
+  t.equal(milesToKm(35), '56.7');
+  t.end();
+});
+
+test('does not bind supplied arguments', function (t) {
   t.plan(3);
   function converter(ratio, input) {
     return (input * ratio).toFixed(1);
   }
   var milesToKm = curry(converter, 1.62);
-  t.equal(milesToKm(35), '56.7');
-  t.equal(milesToKm(10), '16.2');
-  t.equal(milesToKm(10, 35), '16.2');
-  t.end();
-});
-
-test('returns a curried function even if zero currying args are passed', function (t) {
-  t.plan(2);
-  function converter(ratio, input) {
-    return (input * ratio).toFixed(1);
-  }
-  var milesToKm = curry(converter);
-  t.equal(milesToKm(1.62, 35), '56.7');
-  t.equal(milesToKm(1.62)(35), '56.7');
+  t.equal(typeof milesToKm(35), 'function');
+  t.equal(milesToKm(1.62)(10), '16.2');
+  t.equal(milesToKm(1.62, 10), '16.2');
   t.end();
 });
 
@@ -36,7 +36,7 @@ test('executes in the correct context', function (t) {
   function converter(ratio, input) {
     return (input * ratio).toFixed(this.dps);
   }
-  var milesToKm = curry(converter, 1.62);
+  var milesToKm = curry(converter)(1.62);
   t.equal(milesToKm.call({dps: 0}, 35), '57');
   t.equal(milesToKm.call({dps: 2}, 35), '56.70');
   t.end();
@@ -47,10 +47,10 @@ test('converts n-ary function to a sequence of nested unary functions', function
   function addThree(x, y, z) {
     return x + y + z;
   }
-  var addTwoTo2 = curry(addThree, 2);
+  var addTwoTo2 = curry(addThree)(2);
   t.equal(typeof addTwoTo2(3), 'function');
   t.equal(addTwoTo2(5)(8), 15);
   t.equal(addTwoTo2(13, 21), 36);
-  t.equal(curry(addThree, 34, 55, 89), 178);
+  t.equal(curry(addThree)(34)(55)(89), 178);
   t.end();
 });
