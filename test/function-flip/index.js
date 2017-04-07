ea14d0f44287f557b4099044b1c5d6b818fde1cb
chee <chee@snaek.org>
2017-04-07 18:15:13 +0100

    add tests for 0-arity and variadic function-flips

--- test/function-flip/index.js
+++ test/function-flip/index.js
@@ -14,3 +14,21 @@ test('includes the rest of the args in original order', function (t) {
   t.same(flippedArray(2, 3, 5, 8, 13), [3, 2, 5, 8, 13]);
   t.end();
 });
+
+test('does nothing with zero arity functions', function (t) {
+  t.plan(1);
+  function fn() {
+    return 21;
+  }
+  t.equal(21, flip(fn)(34));
+  t.end();
+});
+
+test('still flips the first two args of a variadic function', function (t) {
+  t.plan(1);
+  function pair() {
+    return [].slice.call(arguments, 0, 2);
+  }
+  t.same([55, 89], flip(pair)(89, 55));
+  t.end();
+});
