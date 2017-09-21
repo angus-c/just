2fcec68aa2fd90d28b1541da6a99a58c0e36f90d
Angus Croll <angus@uber.com>
2017-09-21 07:45:56 -0700

    comment

--- javascripts/components/Menu.js
+++ javascripts/components/Menu.js
@@ -49,19 +49,3 @@ export default ({ selected, onSelect }) => {
     </section>
   );
 };
-
-// export default ({onSelect}) => {
-//   return (
-//     <section style={{flex: 1}} className="main-content">
-//       <ul>
-//         <li style={{listStyle: 'none'}}>Objects</li>
-//         <ul>
-//           <li className='link' style={{listStyle: 'none'}} onClick={onSelect}>just-extend</li>
-//         </ul>
-//         <ul>
-//           <li className='link' style={{listStyle: 'none'}} onClick={onSelect}>just-filter-object</li>
-//         </ul>
-//       </ul>
-//     </section>
-//   );
-// }
