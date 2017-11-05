97891694a42ad929c7fab642f5bc1dd9c1701fa8
angus-c <anguscroll@gmail.com>
2017-11-04 17:23:10 -0700

    accept arity argument

--- packages/function-curry/index.js
+++ packages/function-curry/index.js
@@ -1,19 +1,35 @@
 module.exports = curry;
 
 /*
+  function add(a, b, c) {
+    return a + b + c;
+  }
+  curry(add)(1)(2)(3); // 6
+  curry(add)(1)(2)(2); // 5
+  curry(add)(2)(4, 3); // 9
+
+  function add(...args) {
+    return args.reduce((sum, n) => sum + n, 0)
+  }
+  var curryAdd4 = curry(add, 4)
+  curryAdd4(1)(2, 3)(4); // 10
+
   function converter(ratio, input) {
     return (input*ratio).toFixed(1);
   }
-  var curriedConverter = curry(converter)
-  var milesToKm = curriedConverter(1.62);
+  const curriedConverter = curry(converter)
+  const milesToKm = curriedConverter(1.62);
   milesToKm(35); // 56.7
   milesToKm(10); // 16.2
 */
 
-function curry(fn) {
+function curry(fn, arity) {
   return function curried() {
+    if (arity == null) {
+      arity = fn.length;
+    }
     var args = [].slice.call(arguments);
-    if (args.length >= fn.length) {
+    if (args.length >= arity) {
       return fn.apply(this, args);
     } else {
       return function() {
