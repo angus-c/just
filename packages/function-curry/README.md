e3273941453c1daafa1b0c7f860885444b73af72
angus croll <anguscroll@gmail.com>
2017-11-04 17:27:49 -0700

    accept arity argument (#76)

--- packages/function-curry/README.md
+++ packages/function-curry/README.md
@@ -8,10 +8,24 @@ Guilt-free utilities for every occasion.
 ```js
 import curry from 'just-curry-it';
 
+function add(a, b, c) {
+  return a + b + c;
+}
+curry(add)(1)(2)(3); // 6
+curry(add)(1)(2)(2); // 5
+curry(add)(2)(4, 3); // 9
+
+function add(...args) {
+  return args.reduce((sum, n) => sum + n, 0)
+}
+var curryAdd4 = curry(add, 4)
+curryAdd4(1)(2, 3)(4); // 10
+
 function converter(ratio, input) {
   return (input*ratio).toFixed(1);
 }
-const milesToKm = curry(converter)(1.62);
+const curriedConverter = curry(converter)
+const milesToKm = curriedConverter(1.62);
 milesToKm(35); // 56.7
 milesToKm(10); // 16.2
 ```
--- test/function-curry/index.js
+++ test/function-curry/index.js
@@ -1,14 +1,64 @@
 var test = require('../util/test')(__filename);
 var curry = require('../../packages/function-curry');
 
-test('returns a curried function', function(t) {
-  t.plan(1);
+test('converts n-ary function to a sequence of nested unary functions', function(t) {
+  t.plan(13);
+  function addThree(x, y, z) {
+    return x + y + z;
+  }
+  var curryAddThree = curry(addThree);
+  t.equal(typeof curryAddThree(3), 'function');
+  t.equal(typeof curryAddThree(3, 2), 'function');
+  t.equal(typeof curryAddThree(3, 2, 5), 'number');
+  t.equal(typeof curryAddThree(3)(5)(4), 'number');
+  t.equal(curryAddThree(3, 2, 5), 10);
+  t.equal(curryAddThree(3)(5)(4), 12);
+
+  var addTwoTo2 = curry(addThree)(2);
+  t.equal(typeof addTwoTo2(3), 'function');
+  t.equal(typeof addTwoTo2(3)(6), 'number');
+  t.equal(typeof addTwoTo2(3, 6), 'number');
+  t.equal(addTwoTo2(5)(8), 15);
+  t.equal(addTwoTo2(13, 21), 36);
+  t.equal(curry(addThree)(34)(55)(89), 178);
+
   function converter(ratio, input) {
     return (input * ratio).toFixed(1);
   }
   var curriedConverter = curry(converter);
   var milesToKm = curriedConverter(1.62);
   t.equal(milesToKm(35), '56.7');
+  t.end();
+});
+
+test('supports arity override argument', function(t) {
+  t.plan(5);
+  function add(/* infinite number args */) {
+    var args = [].slice.call(arguments);
+    return args.reduce(function(sum, n) {
+      return sum + n;
+    }, 0);
+  }
+  var curryAdd4 = curry(add, 4);
+  t.equal(typeof curryAdd4(3), 'function');
+  t.equal(typeof curryAdd4(3)(1, 4), 'function');
+  t.equal(typeof curryAdd4(3)(1, 4)(2), 'number');
+  t.equal(curryAdd4(5)(8)(2)(7), 22);
+  t.equal(curryAdd4(5)(8, 4)(7), 24);
+  t.end();
+});
+
+test('accepts arguments exceeding arity', function(t) {
+  t.plan(2);
+  function add(/* infinite number args */) {
+    var args = [].slice.call(arguments);
+    return args.reduce(function(sum, n) {
+      return sum + n;
+    }, 0);
+  }
+  var curryAdd4 = curry(add, 4);
+  t.equal(typeof curryAdd4(3)(1, 4)(2, 5), 'number');
+  t.equal(curryAdd4(3)(1, 4)(2, 5), 15);
   t.end();
 });
 
@@ -39,20 +89,5 @@ test('executes in the correct context', function(t) {
   var milesToKm = curry(converter)(1.62);
   t.equal(milesToKm.call({dps: 0}, 35), '57');
   t.equal(milesToKm.call({dps: 2}, 35), '56.70');
-  t.end();
-});
-
-test('converts n-ary function to a sequence of nested unary functions', function(
-  t
-) {
-  t.plan(4);
-  function addThree(x, y, z) {
-    return x + y + z;
-  }
-  var addTwoTo2 = curry(addThree)(2);
-  t.equal(typeof addTwoTo2(3), 'function');
-  t.equal(addTwoTo2(5)(8), 15);
-  t.equal(addTwoTo2(13, 21), 36);
-  t.equal(curry(addThree)(34)(55)(89), 178);
   t.end();
 });
