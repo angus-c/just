583bc9a494db4f1fb478ef4a6b118a53abf5c387
angus croll <anguscroll@gmail.com>
2017-11-20 12:51:10 -0800

    Update index.js

--- packages/array-intersect/index.js
+++ packages/array-intersect/index.js
@@ -2,7 +2,7 @@ module.exports = intersect;
 
 /*
   intersect([1, 2, 5, 6], [2, 3, 5, 6]); // [2, 5, 6]
-  intersect([1, 2, 2, 4, 5], [3, 2, 2, 5, 7]); // [2, 5]  
+  intersect([1, 2, 2, 4, 5], [3, 2, 2, 5, 7]); // [2, 5]
 */
 
 function intersect(arr1, arr2) {
