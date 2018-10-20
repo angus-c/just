f07981e841b9ca67a3b43c371744464b55b5d8a3
angus croll <anguscroll@gmail.com>
2018-10-20 15:42:35 -0700

    camelCase 4.0 (#112)

--- test/string-camel-case/index.js
+++ test/string-camel-case/index.js
@@ -1,21 +1,14 @@
 var test = require('../util/test')(__filename);
 var camelCase = require('../../packages/string-camel-case');
 
-test('string with spaces (no strict)', function(t) {
+test('string with spaces', function(t) {
   t.plan(2);
   t.equal(camelCase('the quick brown fox'), 'theQuickBrownFox');
   t.equal(camelCase('the quick     brownfox'), 'theQuickBrownfox');
   t.end();
 });
 
-test('string with spaces (with strict)', function(t) {
-  t.plan(2);
-  t.equal(camelCase('the quick brown fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('the quick     brownfox', {strict: true}), 'theQuickBrownfox');
-  t.end();
-});
-
-test('string with punctuation (no strict)', function(t) {
+test('string with punctuation', function(t) {
   t.plan(4);
   t.equal(camelCase('the_quick_brown_fox'), 'theQuickBrownFox');
   t.equal(camelCase('the-quick-brown-fox'), 'theQuickBrownFox');
@@ -24,46 +17,27 @@ test('string with punctuation (no strict)', function(t) {
   t.end();
 });
 
-test('string with punctuation (with strict)', function(t) {
-  t.plan(4);
-  t.equal(camelCase('the_quick_brown_fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('the-quick-brown-fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('the*quick-brown_fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('the****quick-_-brown_:fox', {strict: true}), 'theQuickBrownFox');
-  t.end();
-});
-
-test('string with mixed spaces and punctuation (no strict)', function(t) {
+test('string with mixed spaces and punctuation', function(t) {
   t.plan(2);
   t.equal(camelCase('the_quick_brown_   fox'), 'theQuickBrownFox');
   t.equal(camelCase('the** **quick-_-brown_:fox'), 'theQuickBrownFox');
   t.end();
 });
 
-test('string with mixed spaces and punctuation (with strict)', function(t) {
-  t.plan(2);
-  t.equal(camelCase('the_quick_brown_   fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('the** **quick-_-brown_:fox', {strict: true}), 'theQuickBrownFox');
-  t.end();
-});
-
-test('string with existing capitalization (no strict)', function(t) {
-  t.plan(5);
+test('string with existing capitalization', function(t) {
+  t.plan(12);
   t.equal(camelCase('theQuickBrownFox'), 'theQuickBrownFox');
+  t.equal(camelCase('TheQuickBrownFox'), 'theQuickBrownFox');
   t.equal(camelCase('the Quick Brown Fox'), 'theQuickBrownFox');
-  t.equal(camelCase('The quick brown FOX'), 'TheQuickBrownFOX');
+  t.equal(camelCase('The quick brown FOX'), 'theQuickBrownFox');
+  t.equal(camelCase('theQUICKBrownFox'), 'theQuickBrownFox');
   t.equal(camelCase('behold theQuickBrownFox'), 'beholdTheQuickBrownFox');
-  t.equal(camelCase('Behold theQuickBrownFox'), 'BeholdTheQuickBrownFox');
+  // camel-case all-caps substrings if length >= 4
+  t.equal(camelCase('Behold theQUickBrownFox'), 'beholdTheQUickBrownFox');
+  t.equal(camelCase('Behold theQUIckBrownFox'), 'beholdTheQUIckBrownFox');
+  t.equal(camelCase('Behold theQUICkBrownFox'), 'beholdTheQuiCkBrownFox');
+  t.equal(camelCase('Behold theQUICKBrownFox'), 'beholdTheQuickBrownFox');
+  t.equal(camelCase('THE_QUICK_BROWN_FOX'), 'theQuickBrownFox');
+  t.equal(camelCase('THE_QUICK/BROWN_FOX'), 'theQuickBrownFox');
   t.end();
 });
-
-test('string with existing capitalization (with strict)', function(t) {
-  t.plan(5);
-  t.equal(camelCase('theQuickBrownFox', {strict: true}), 'thequickbrownfox');
-  t.equal(camelCase('the Quick Brown Fox', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('The quick brown FOX', {strict: true}), 'theQuickBrownFox');
-  t.equal(camelCase('behold theQuickBrownFox', {strict: true}), 'beholdThequickbrownfox');
-  t.equal(camelCase('Behold theQuickBrownFox', {strict: true}), 'beholdThequickbrownfox');
-  t.end();
-});
-
