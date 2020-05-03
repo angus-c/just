39cb6451dc37a2ee7eddec628a41042a6082d4fa
angus croll <anguscroll@gmail.com>
2020-05-03 13:32:09 -0700

    don't shuffle all by default (#187)

--- test/array-shuffle/index.js
+++ test/array-shuffle/index.js
@@ -3,22 +3,40 @@ var shuffle = require('../../packages/array-shuffle');
 var compare = require('../../packages/collection-compare');
 
 test('returns elements, randomly sorted', function(t) {
-  t.plan(2);
-  var arr = [1, 2, 3, 4, 5];
+  t.plan(3);
+  var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
   var shuffled = shuffle(arr);
   t.equal(arr.length, shuffled.length);
+  t.notOk(compare(arr, shuffled));
   t.ok(compare(arr.sort(), shuffled.sort()));
   t.end();
 });
 
-test('returns duplicate when array has 0 or 1 element', function(t) {
-  t.plan(2);
+test('always returns duplicate when array has 0 or 1 elements', function(t) {
+  t.plan(4);
   var arr = [1];
   var shuffled = shuffle(arr);
   t.ok(compare(arr, shuffled));
   var arr2 = [];
   var shuffled2 = shuffle(arr2);
   t.ok(compare(arr2, shuffled2));
+  var shuffled3 = shuffle(arr, {shuffleAll: true});
+  t.ok(compare(arr, shuffled3));
+  var shuffled4 = shuffle(arr2, {shuffleAll: true});
+  t.ok(compare(arr2, shuffled4));
+  t.end();
+});
+
+test('all elements moved when shuffleAll is `true`', function(t) {
+  var arr = [1, 2, 3, 4, 5];
+  t.plan(3 + arr.length);
+  var shuffled = shuffle(arr, {shuffleAll: true});
+  t.equal(arr.length, shuffled.length);
+  t.notOk(compare(arr, shuffled));
+  arr.forEach((each, i) => {
+    t.notEqual(each, shuffled[i]);
+  });
+  t.ok(compare(arr.sort(), shuffled.sort()));
   t.end();
 });
 
