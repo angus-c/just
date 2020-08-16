77cc1e3aef631e412dc7dbfc20d446f29474ab36
Max Strübing <mxstrbng@gmail.com>
2020-08-16 11:39:20 +0200

    refactor: use `Object.values` in `object-values`

--- packages/object-values/index.js
+++ packages/object-values/index.js
@@ -14,17 +14,11 @@ module.exports = values;
 */
 
 function values(obj) {
-  var result = [];
   if (Array.isArray(obj)) {
     return obj.slice(0);
   }
   if (typeof obj == 'object' || typeof obj == 'function') {
-    var keys = Object.keys(obj);
-    var len = keys.length;
-    for (var i = 0; i < len; i++) {
-      result.push(obj[keys[i]]);
-    }
-    return result;
+    return Object.values(obj);
   }
   throw new Error('argument to `values` must be an object');
 }
