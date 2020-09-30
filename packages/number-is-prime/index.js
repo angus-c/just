3c3a3ff10f1ba67a8a0f7ff6552cd6431936ab46
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-09-30 21:52:12 +0200

    create is-prime package

---
+++ packages/number-is-prime/index.js
@@ -0,0 +1,24 @@
+module.exports = isPrime;
+
+/*
+  isPrime(1); // false
+  isPrime(2); // true
+  isPrime(17); // true
+  isPrime(10); // true
+*/
+
+function isPrime(number) {
+  if (!Number.isInteger(number)) {
+    throw new Error('Expected an integer');
+  }
+
+  if (number < 2) {
+    return false;
+  }
+
+  for (var i = 2; i < Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+
+  return true;
+}
