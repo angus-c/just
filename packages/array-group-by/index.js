5d66c9719770ab167356b236e7293d5d5e093489
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-10-05 22:53:57 +0200

    refactor group by function

--- packages/array-group-by/index.js
+++ packages/array-group-by/index.js
@@ -1,25 +1,32 @@
 module.exports = groupBy;
 
+/*
+  groupBy([6.1, 4.2, 6.3], Math.floor); // { '4': [4.2], '6': [6.1, 6.3] }
+  groupBy([1,2,3,4,5,6], function(i) { return i % 2 }); // { '0': [2, 4, 6], '1': [1, 3, 5] }
+*/
+
 function groupBy(arr, cb) {
   if (!Array.isArray(arr)) {
-    throw new Error('expected an array for first argument');
+    throw new Error('just-group-by expects an array for first argument');
   }
 
   if (typeof cb !== 'function') {
-    throw new Error('expected a function for second argument');
+    throw new Error('just-group-by expects a function for second argument');
   }
 
   var result = {};
-  for (var i = 0; i < arr.length; i++) {
+  var size = arr.length;
+
+  for (var i = 0; i < size; i++) {
     var item = arr[i];
     var bucketCategory = cb(item);
     var bucket = result[bucketCategory];
 
-    if (!Array.isArray(bucket)) {
-      result[bucketCategory] = [item];
-    } else {
-      result[bucketCategory].push(item);
+    if (!bucket) {
+      result[bucketCategory] = [];
     }
+
+    result[bucketCategory].push(item);
   }
 
   return result;
