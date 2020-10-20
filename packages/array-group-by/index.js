49412245fc43d1731b3779afa0bf33e52197ff93
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-10-20 20:10:56 +0200

    revert group-by changes

--- packages/array-group-by/index.js
+++ packages/array-group-by/index.js
@@ -1,32 +1,25 @@
 module.exports = groupBy;
 
-/*
-  groupBy([6.1, 4.2, 6.3], Math.floor); // { '4': [4.2], '6': [6.1, 6.3] }
-  groupBy([1,2,3,4,5,6], function(i) { return i % 2 }); // { '0': [2, 4, 6], '1': [1, 3, 5] }
-*/
-
 function groupBy(arr, cb) {
   if (!Array.isArray(arr)) {
-    throw new Error('just-group-by expects an array for first argument');
+    throw new Error('expected an array for first argument');
   }
 
   if (typeof cb !== 'function') {
-    throw new Error('just-group-by expects a function for second argument');
+    throw new Error('expected a function for second argument');
   }
 
   var result = {};
-  var size = arr.length;
-
-  for (var i = 0; i < size; i++) {
+  for (var i = 0; i < arr.length; i++) {
     var item = arr[i];
     var bucketCategory = cb(item);
     var bucket = result[bucketCategory];
 
-    if (!bucket) {
-      result[bucketCategory] = [];
+    if (!Array.isArray(bucket)) {
+      result[bucketCategory] = [item];
+    } else {
+      result[bucketCategory].push(item);
     }
-
-    result[bucketCategory].push(item);
   }
 
   return result;
