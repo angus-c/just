fcd5787cf19eb634a51a1a20bbbbb47c94ab8114
luci-m-666 <72810846+luci-m-666@users.noreply.github.com>
2020-10-28 16:01:21 +0530

    Fix prototype pollution

--- packages/object-safe-set/index.js
+++ packages/object-safe-set/index.js
@@ -4,19 +4,15 @@ module.exports = set;
   var obj1 = {};
   set(obj1, 'a.aa.aaa', 4); // true
   obj1; // {a: {aa: {aaa: 4}}}
-
   var obj2 = {};
   set(obj2, ['a', 'aa', 'aaa'], 4); // true
   obj2; // {a: {aa: {aaa: 4}}}
-
   var obj3 = {a: {aa: {aaa: 2}}};
   set(obj3, 'a.aa.aaa', 3); // true
   obj3; // {a: {aa: {aaa: 3}}}
-
   // don't clobber existing
   var obj4 = {a: {aa: {aaa: 2}}};
   set(obj4, 'a.aa', {bbb: 7}); // false
-
   const obj5 = {a: {}};
   const sym = Symbol();
   set(obj5.a, sym, 7); // true
@@ -36,6 +32,9 @@ function set(obj, props, value) {
   }
   var thisProp;
   while ((thisProp = props.shift())) {
+    if (thisProp === '__proto__' || thisProp === 'constructor' || thisProp === 'prototype') {
+      return false;
+    }
     if (typeof obj[thisProp] == 'undefined') {
       obj[thisProp] = {};
     }
