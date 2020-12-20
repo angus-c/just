032144a26187bd70b2d8e5b7333e66e580060982
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-12-20 12:24:33 +0100

    update array-product tests

--- test/array-product/index.js
+++ test/array-product/index.js
@@ -2,7 +2,7 @@ var test = require('../util/test')(__filename);
 var product = require('../../packages/array-product');
 
 test('products', function(t) {
-  t.plan(5);
+  t.plan(6);
 
   t.deepEqual(
     product([]),
@@ -22,6 +22,11 @@ test('products', function(t) {
   t.deepEqual(
     product([['a', 'b'], [1, 2]]),
     [['a', 1], ['a', 2], ['b', 1], ['b', 2]]
+  );
+
+  t.deepEqual(
+    product([[1, 2], ['a', 'b', 'c']]),
+    [[1, 'a'], [1, 'b'], [1, 'c'], [2, 'a'], [2, 'b'], [2, 'c']]
   );
 
   t.deepEqual(
