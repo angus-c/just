ea28c3970b933ae451a05d1282cc8e25a4d3ce98
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-03-17 21:31:16 +0100

    throws an error if resolver is not a function

--- packages/function-memoize/index.js
+++ packages/function-memoize/index.js
@@ -1,6 +1,33 @@
 module.exports = memoize;
 
+/*
+  const sumByOne = memoize(function(value) {
+    return value + 1;
+  });
+
+  sumByOne(10);
+  sumByOne(10); -- Cache hit!
+
+  sumByOne(20);
+  sumByOne(20); -- Cache hit!
+
+  // Custom cache
+  var sum = memoize(function(a, b) {
+    return a + b
+  }, function(a, b) {
+    return `${a}-${b}`
+  })
+
+  sum(10, 10)
+  sum(10, 20)
+  sum(10, 20) -- Cache hit!
+*/
+
 function memoize(callback, resolver) {
+  if (resolver !== undefined && typeof resolver !== 'function') {
+    throw new Error('`resolver` should be a function');
+  }
+
   var cache = {};
 
   var memoized = function() {
--- test/function-memoize/index.js
+++ test/function-memoize/index.js
@@ -1,10 +1,10 @@
 var test = require('../util/test')(__filename);
 var memoize = require('../../packages/function-memoize');
 
-test('memoize with one parameter', function (t) {
+test('memoize with one parameter', function(t) {
   t.plan(3);
 
-  var calc = memoize(function (a) {
+  var calc = memoize(function(a) {
     return a + 1;
   });
 
@@ -18,12 +18,12 @@ test('memoize with one parameter', function (t) {
   t.end();
 });
 
-test('memoize with two parameter', function (t) {
+test('memoize with two parameter', function(t) {
   t.plan(3);
 
-  var calc = memoize(function (a, b) {
+  var calc = memoize(function(a, b) {
     return a + b;
-  }, function (a, b) {
+  }, function(a, b) {
     return [a, b].join('-');
   });
 
@@ -36,12 +36,12 @@ test('memoize with two parameter', function (t) {
   t.end();
 });
 
-test('get result from cache using customized key', function (t) {
+test('get result from cache using customized key', function(t) {
   t.plan(5);
 
-  var concat = memoize(function (a, b, c) {
+  var concat = memoize(function(a, b, c) {
     return a + b + c;
-  }, function () {
+  }, function() {
     return 'myKey';
   });
 
@@ -54,6 +54,32 @@ test('get result from cache using customized key', function (t) {
   var result2 = concat('d', 'e', 'f');
   t.deepEqual(concat.cache, {'myKey': 'abc'});
   t.equal(result2, 'abc');
+
+  t.end();
+});
+
+test('throws an error when `resolver` is not a function', function(t) {
+  t.plan(4);
+
+  var callback = function(a, b) {
+    return a + b;
+  };
+
+  t.throws(function() {
+    memoize(callback, true);
+  });
+
+  t.throws(function() {
+    memoize(callback, [1, 2]);
+  });
+
+  t.throws(function() {
+    memoize(callback, 'myResolver');
+  });
+
+  t.throws(function() {
+    memoize(callback, 123);
+  });
 
   t.end();
 });
