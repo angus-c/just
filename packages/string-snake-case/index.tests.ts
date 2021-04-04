c9dd5ccdc7d028718ee0c246c204b4182641186f
angus-c <anguscroll@gmail.com>
2021-04-04 12:49:39 -0700

    just-snake-case

---
+++ packages/string-snake-case/index.tests.ts
@@ -0,0 +1,18 @@
+import snakeCase = require("./index");
+
+// OK
+snakeCase('myString');
+
+// Not OK
+// @ts-expect-error
+snakeCase(); 
+// @ts-expect-error
+snakeCase(0); 
+// @ts-expect-error
+snakeCase([]); 
+// @ts-expect-error
+snakeCase({}); 
+// @ts-expect-error
+snakeCase(/nope/); 
+// @ts-expect-error
+snakeCase(false); 
