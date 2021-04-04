0f5641baa05d01bbfb93b7b6cf54f166f0cfabb0
angus-c <anguscroll@gmail.com>
2021-04-04 11:59:21 -0700

    just-safe-set

---
+++ packages/object-safe-set/index.d.ts
@@ -0,0 +1,3 @@
+// Definitions by: Roman Lerchster <https://github.com/wa4-fearless-otter>
+declare function set(item: any[] | {}, target: string | symbol | Array<string | symbol>, value: any): boolean;
+export = set;
---
+++ packages/object-safe-set/index.tests.ts
@@ -0,0 +1,37 @@
+import set = require("./index");
+
+const arr = ['one', 'two'];
+const obj = {foo: {bar: arr}};
+
+// OK
+set(arr, '0', {}); 
+set(arr, ['0'], ''); 
+set(obj, 'foo.bar', 0); 
+set(obj, 'foo.bar.0', 1); 
+set(obj, 'foo.bar', {}); 
+set(obj, 'foo.bar.0', {}); 
+set(obj, 'foo.bar', function(){}); 
+set(obj, 'foo.bar.0', function(){}); 
+
+
+// Not OK
+// @ts-expect-error
+set(); 
+// @ts-expect-error
+set(obj); 
+// @ts-expect-error
+set([]); 
+// @ts-expect-error
+set({}); 
+// @ts-expect-error
+set(obj, 3); 
+// @ts-expect-error
+set(obj, [3]); 
+// @ts-expect-error
+set(false); 
+// @ts-expect-error
+set(null); 
+// @ts-expect-error
+set(undefined); 
+// @ts-expect-error
+set(obj, 'foo.bar'); 
