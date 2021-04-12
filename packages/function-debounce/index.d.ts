b8c1a70056bb16a989228a808272848250a98552
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-04-12 02:15:40 +0200

    Add cancel method to the debounce function (#227)
    * add cancel method to debounce
    
    * update debounce types
    
    * fix test issue
    
    * add new test case
    
    * update index.tests.ts

--- packages/function-debounce/index.d.ts
+++ packages/function-debounce/index.d.ts
@@ -1,22 +1,24 @@
 // Definitions by: Aziz Khambati <https://github.com/azizhk>
 type ArgumentTypes<F extends Function> = F extends (...args: infer A) => any ? A : never;
+type CancelType = () => void;
 
 export = debounce;
 
-declare function debounce<T extends Function>(
-    fn: T,
-    wait?: 0,
-    callFirst?: boolean
-): T;
 
 declare function debounce<T extends Function>(
-    fn: T,
-    wait: number,
-    callFirst: true
-): T;
+  fn: T,
+  wait?: 0,
+  callFirst?: boolean
+): T & {cancel: CancelType};
 
 declare function debounce<T extends Function>(
-    fn: T,
-    wait: number,
-    callFirst?: false
-): (...args: ArgumentTypes<T>) => void;
+  fn: T,
+  wait: number,
+  callFirst: true
+): T & {cancel: CancelType};
+
+declare function debounce<T extends Function>(
+  fn: T,
+  wait: number,
+  callFirst?: false
+): ((...args: ArgumentTypes<T>) => void) & {cancel: CancelType};
