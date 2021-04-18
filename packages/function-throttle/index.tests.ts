f94805fdde6df9b76999f36467e480c5d78e82e4
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-04-18 12:21:31 +0200

    update throttle types

--- packages/function-throttle/index.tests.ts
+++ packages/function-throttle/index.tests.ts
@@ -3,13 +3,17 @@ import throttle = require('./index');
 //OK
 
 throttle(() => 'foo', 0);
-throttle(() => 'foo', 100); 
+throttle(() => 'foo', 100);
 
-throttle(() => 'foo', 200, {}); 
+throttle(() => 'foo', 200, {});
 throttle(() => 'foo', 300, {leading: false, trailing: false});
 throttle(() => 'foo', 400, {leading: true, trailing: false});
 throttle(() => 'foo', 500, {leading: false, trailing: true});
 throttle(() => 'foo', 600, {leading: true, trailing: true});
+
+throttle(() => 'foo', 100).cancel();
+throttle(() => 'foo', 200, {}).cancel();
+throttle(() => 'foo', 400, {leading: true, trailing: false}).cancel();
 
 // not OK
 
