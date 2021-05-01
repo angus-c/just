9d5ce219ddfd055f39c6484e4225b17e80e99a56
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-05-01 20:44:40 +0200

    update array-flatten adding depth param

--- packages/array-flatten/index.js
+++ packages/array-flatten/index.js
@@ -5,19 +5,37 @@ module.exports = flatten;
   // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 */
 
-function flatten(arr) {
-  if (!Array.isArray(arr)) {
-    throw new Error('expected an array');
-  }
+function flattenHelper(arr, depth) {
   var result = [];
   var len = arr.length;
+
   for (var i = 0; i < len; i++) {
     var elem = arr[i];
-    if (Array.isArray(elem)) {
-      result.push.apply(result, flatten(elem));
+
+    if (Array.isArray(elem) && depth > 0) {
+      result.push.apply(result, flattenHelper(elem, depth - 1));
     } else {
       result.push(elem);
     }
   }
+
   return result;
+}
+
+function flatten(arr, depth) {
+  if (!Array.isArray(arr)) {
+    throw new Error('expected an array');
+  }
+
+  if (depth !== undefined && typeof depth !== 'number') {
+    throw new Error('depth expects a number');
+  }
+
+  var optionDepth = typeof depth === 'number' ? depth : Infinity;
+
+  if (Array.prototype.flat) {
+    return arr.flat(optionDepth);
+  }
+
+  return flattenHelper(arr, optionDepth);
 }
