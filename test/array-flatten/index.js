665daf3e7854ef9ad3f58ebeae1302eb23c22dd2
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-05-01 21:04:59 +0200

    update array-flatten tests

--- test/array-flatten/index.js
+++ test/array-flatten/index.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-extend-native */
+
 var test = require('../util/test')(__filename);
 var flatten = require('../../packages/array-flatten');
+
+var protoFlat = Array.prototype.flat;
+Array.prototype.flat = null;
 
 test('flattened arrays are unchanged', function(t) {
   t.plan(1);
@@ -30,3 +35,45 @@ test('throws on non-arrays return an empty array', function(t) {
   });
   t.end();
 });
+
+test('returns a new array with all sub-arrays items concatenated into it up to the specific depth',
+  function(t) {
+    t.plan(5);
+
+    var arr = [1, [2, [3, [4, [5]]]]];
+    t.deepEqual(flatten(arr, 0), arr);
+    t.deepEqual(flatten(arr, 1), [1, 2, [3, [4, [5]]]]);
+    t.deepEqual(flatten(arr, 2), [1, 2, 3, [4, [5]]]);
+    t.deepEqual(flatten(arr, 3), [1, 2, 3, 4, [5]]);
+    t.deepEqual(flatten(arr, 4), [1, 2, 3, 4, 5]);
+
+    t.end();
+  });
+
+test('throws when depth is not a number', function(t) {
+  t.plan(5);
+
+  t.throws(function() {
+    flatten([1, 2, 3], 'depth');
+  });
+
+  t.throws(function() {
+    flatten([1, 2, 3], null);
+  });
+
+  t.throws(function() {
+    flatten([1, 2, 3], {});
+  });
+
+  t.throws(function() {
+    flatten([1, 2, 3], []);
+  });
+
+  t.throws(function() {
+    flatten([1, 2, 3], function() {});
+  });
+
+  t.end();
+});
+
+Array.prototype.flat = protoFlat;
