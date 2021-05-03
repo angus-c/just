bdd29cbae6f43c2ef012356151583d66457bb60f
Peter Safranek <pe8ter@mac.com>
2021-05-03 14:53:54 -0700

    Add typings for object-merge
    I copied these typings from the DefinitelyTyped repository.

---
+++ packages/object-merge/index.tests.ts
@@ -0,0 +1,54 @@
+import merge = require('./index');
+
+// OK
+
+// Single argument
+merge({});
+merge({ a: 1 });
+merge([]);
+merge(() => {});
+merge(new Map());
+merge(new Set());
+
+// Multiple arguments
+merge({}, {});
+merge({}, { a: 1 });
+merge({}, []);
+merge({}, () => {});
+merge({}, new Map());
+merge({}, new Set());
+merge({}, { a: 1 }, { a: 2, b: 3 });
+
+// Not OK
+
+// Single argument
+// @ts-expect-error
+merge();
+// @ts-expect-error
+merge(0);
+// @ts-expect-error
+merge('');
+// @ts-expect-error
+merge(false);
+// @ts-expect-error
+merge(null);
+// @ts-expect-error
+merge(undefined);
+// @ts-expect-error
+merge(Symbol());
+
+// Multiple arguments
+// @ts-expect-error
+merge({}, 0);
+// @ts-expect-error
+merge({}, '');
+// @ts-expect-error
+merge({}, false);
+// @ts-expect-error
+merge({}, null);
+// @ts-expect-error
+merge({}, undefined);
+// @ts-expect-error
+merge({}, Symbol());
+// @ts-expect-error
+merge({}, {}, null);
