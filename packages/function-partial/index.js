2be75e3d9111651e2ff18f31c41e57f4101264d8
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-05-03 22:27:26 +0200

    improve logic to get items from args

--- packages/function-partial/index.js
+++ packages/function-partial/index.js
@@ -10,17 +10,23 @@ module.exports = partial;
 
 function partial(fn /*, arg1, arg2 etc */) {
   var partialArgs = [].slice.call(arguments, 1);
+
   if (!partialArgs.length) {
     return fn;
   }
+
   return function() {
     var args = [].slice.call(arguments);
+    var argsIndex = 0;
     var derivedArgs = [];
+
     for (var i = 0; i < partialArgs.length; i++) {
       var thisPartialArg = partialArgs[i];
+
       derivedArgs[i] =
-        thisPartialArg === undefined ? args.shift() : thisPartialArg;
+        thisPartialArg === undefined ? args[argsIndex++] : thisPartialArg;
     }
+
     return fn.apply(this, derivedArgs.concat(args));
   };
 }
