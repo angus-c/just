64fa30c32bdf5e0fa2d504677fadc9343c6136ed
Daniel Imfeld <daniel@imfeld.dev>
2021-05-25 01:13:44 +0000

    Add type definition for array-range

---
+++ packages/array-range/index.d.ts
@@ -0,0 +1,3 @@
+declare function range(start: number, end: number|null, step?: number|null): number[];
+declare function range(end: number, step?: number|null): number[];
+export = range;
---
+++ packages/array-range/index.tests.ts
@@ -0,0 +1,26 @@
+import range = require('./index');
+
+// OK
+range(5);
+range(5, 6);
+range(10, 50, 10);
+range(5, null);
+range(5, null, null);
+range(5, 10, null);
+
+// Not OK
+// @ts-expect-error
+range(null);
+// @ts-expect-error
+range(5, "abc");
+// @ts-expect-error
+range(5, true);
+// @ts-expect-error
+range(5, 10, true);
+// @ts-expect-error
+range("abc");
+// @ts-expect-error
+range(5, 10, 15, 20);
+// @ts-expect-error
+range();
+
