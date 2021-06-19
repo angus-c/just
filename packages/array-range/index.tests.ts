f5d1780fa65962bcdb1f11e55e264b105bfc70fd
Daniel Imfeld <daniel@imfeld.dev>
2021-06-19 13:35:58 -1000

    Add type definitions for array-range (#275)
    * Add type definition for array-range
    
    * update for PR feedback
    
    Co-authored-by: angus croll <anguscroll@gmail.com>

---
+++ packages/array-range/index.tests.ts
@@ -0,0 +1,25 @@
+import range = require('./index');
+
+// OK
+range(5);
+range(5, 6);
+range(10, 50, 10);
+range(5, null);
+range(5, null, null);
+range(5, 10, null);
+range();
+
+// Not OK
+// @ts-expect-error
+range(5, "abc");
+// @ts-expect-error
+range(5, true);
+// @ts-expect-error
+range(5, 10, true);
+// @ts-expect-error
+range("abc");
+// @ts-expect-error
+range(null);
+// @ts-expect-error
+range(5, 10, 15, 20);
+
