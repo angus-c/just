78c57cafd30cb81080f47b740a835ae3682d2cf0
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2021-08-09 10:43:04 +0200

    implements specific solution for environments that don't implement Map

--- packages/function-memoize/index.js
+++ packages/function-memoize/index.js
@@ -23,20 +23,53 @@ module.exports = memoize;
   sum(10, 20) -- Cache hit!
 */
 
-function memoize(callback, options, resolver) {
+function Order() {
+  this.keys = [];
+  this.cache = {};
+}
+
+Order.prototype = {
+  'has': function(key) {
+    return this.cache.hasOwnProperty(key);
+  },
+
+  'get': function(key) {
+    return this.cache[key];
+  },
+
+  'set': function(key, value) {
+    if (!this.has(key)) {
+      this.keys.push(key);
+    }
+
+    this.cache[key] = value;
+  },
+
+  'delete': function(key) {
+    var index = this.keys.indexOf(key);
+    this.keys.splice(index, 1);
+    delete this.cache[key];
+  },
+
+  'keys': function() {
+    return this.keys;
+  },
+};
+
+function memoize(callback, resolver, options) {
   if (typeof callback !== 'function') {
     throw new Error('`callback` should be a function');
-  }
-
-  if (options !== undefined && typeof options !== 'object') {
-    throw new Error('`options` should be an object');
   }
 
   if (resolver !== undefined && typeof resolver !== 'function') {
     throw new Error('`resolver` should be a function');
   }
 
-  var cache = new Map();
+  if (options !== undefined && typeof options !== 'object') {
+    throw new Error('`options` should be an object');
+  }
+
+  var cache = 'Map' in (typeof window === 'undefined' ? global : window) ? new Map() : new Order();
 
   var memoized = function() {
     var args = Array.prototype.slice.call(arguments); // to simplify JSON.stringify
