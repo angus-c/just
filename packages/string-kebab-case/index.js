26809c9265e620595cb962d0ce365edb9c9886b0
angus-c <anguscroll@gmail.com>
2021-10-20 19:00:07 -0700

    Treat consecutive caps as words, add international letters

--- packages/string-kebab-case/index.js
+++ packages/string-kebab-case/index.js
@@ -14,24 +14,20 @@ module.exports = kebabCase;
 // any combination of spaces and punctuation characters
 // thanks to http://stackoverflow.com/a/25575009
 var wordSeparators = /[\s\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]+/;
-var capitals_plus_lower = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]+[a-z]/g;
-var capitals_plus_other = /[A-Z\u00C0-\u00D6\u00D9-\u00DD]+[$|^a-zA-Z]/g;
+var capital_plus_lower = /[A-ZÀ-Ý\u00C0-\u00D6\u00D9-\u00DD][a-zà-ÿ]/g;
+var capitals = /[A-ZÀ-Ý\u00C0-\u00D6\u00D9-\u00DD]+/g;
 
 function kebabCase(str) {
-  //replace consecutive caps of caps with space + lower case equivalent for later parsing
-  str = str.replace(capitals_plus_lower, function(match) {
-    // match is consecutive caps followed by one non-cap
-    console.log('***** match', match);
-    var lastChar = match[match.length - 1];
-    var lastCapLowered = match[match.length - 2].toLowerCase();
-    var leadingChars = '';
-    if (match.length > 2) {
-      var leadingChars = ' ' + match.slice(0, match.length - 2).toLowerCase();
-    }
-    return leadingChars + ' ' + lastCapLowered + lastChar;
+  // replace word starts with space + lower case equivalent for later parsing
+  // 1) treat cap + lower as start of new word
+  str = str.replace(capital_plus_lower, function(match) {
+    // match is one caps followed by one non-cap
+    return ' ' + (match[0].toLowerCase() || match[0]) + match[1];
   });
-  str = str.replace(capitals_plus_other, function(match) {
-    return match.toLowerCase();
+  // 2) treat all remaining capitals as words
+  str = str.replace(capitals, function(match) {
+    // match is a series of caps
+    return ' ' + match.toLowerCase();
   });
   return str
     .trim()
