f6076d19c26bea10773d879711a3d1ce30a31e2f
Raul Melo <melo.raulf@gmail.com>
2021-10-31 08:13:18 +0100

    feat(just-cartesian-product): enable esm (#315)
    * feat(just-cartesian-product): enable esm
    
    * chore: add rollup as comparison
    
    * chore: remove esbuild
    
    * feat: create rollup config factory
    
    * chore: replace index.esm with rollup output
    
    * chore: create rollup config for all packages
    
    * temp npmrc
    
    * feat: add support to ESM
    
    BREAKING CHANGE: add type: commonjs, module and exports in package.json
    
    * chore: remove .npmrc

---
+++ packages/array-percentile/index.esm.js
@@ -0,0 +1,44 @@
+var arrayPercentile = percentile;
+
+var nonNumericMsg = 'all values passed to `percentile` must be numeric';
+
+// Using linear interpolation method
+// https://en.wikipedia.org/wiki/Percentile
+
+function percentile(arr, percentileValue) {
+  if (!Array.isArray(arr)) {
+    throw new Error('the argument to `percentile` must be an array');
+  }
+  if (!arr.length) {
+    throw new Error('no values were passed to `percentile`');
+  }
+  if (arr.length == 1) {
+    if (Number.isFinite(arr[0])) {
+      return arr[0];
+    } else {
+      throw new Error(nonNumericMsg);
+    }
+  }
+  var sorted = arr.sort(function(a, b) {
+    if (!Number.isFinite(a)) {
+      throw new Error(nonNumericMsg);
+    }
+    return a >= b ? 1 : -1;
+  });
+  var percentileRank = Math.min(
+    Math.max((arr.length * percentileValue) / 100 - 0.5, 0),
+    arr.length - 1
+  );
+  var lowerInt = Math.floor(percentileRank);
+  if (percentileRank == lowerInt) {
+    return sorted[lowerInt];
+  } else {
+    var upperInt = Math.ceil(percentileRank);
+    return (
+      sorted[lowerInt] +
+      (percentileRank - lowerInt) * (sorted[upperInt] - sorted[lowerInt])
+    );
+  }
+}
+
+export { arrayPercentile as default };
