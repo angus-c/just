81ca73eaaa518d5605871de08cde1b59cf295b18
hmmhmmhm <hmmhmmhm@naver.com>
2021-11-01 01:31:16 +0900

    More simple random function

--- packages/number-random-integer/index.js
+++ packages/number-random-integer/index.js
@@ -1,24 +1,7 @@
 module.exports = random;
 
-function random(lower, upper) {
-  if (lower !== undefined && typeof lower !== 'number') {
-    throw new Error('lower should be a number');
-  }
-
-  if (upper !== undefined && typeof upper !== 'number') {
-    throw new Error('upper should be a number');
-  }
-
-  var from = (typeof lower === 'number' ? lower : 1) + 1;
-  var end = upper;
-  var floor = Math.floor;
-  var random = Math.random;
-
-  if (end === undefined) {
-    return floor(random() * (from));
-  }
-
-  return floor(
-    (random() * (end - from)) + from
-  );
+function random(min, max) {
+  if (typeof min !== "number" || typeof max !== "number")
+    throw new Error("random: min and max must be numbers");
+  return ~~(Math.random() * (++max - min)) + min;
 }
