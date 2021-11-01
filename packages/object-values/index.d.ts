abb8601e1c7b1676fd7e9951d5a03a02288fa577
Brandon Ryan <bjryan19@protonmail.com>
2021-10-31 23:15:51 -0500

    added strict typings for tests

--- packages/object-values/index.d.ts
+++ packages/object-values/index.d.ts
@@ -1,6 +1,6 @@
-declare function values<Arr extends unknown[]>(obj: Arr): Arr;
-declare function values<Obj extends object>(obj: Obj): Obj extends String ? Obj[number][]
-	: Obj extends Number | Boolean ? unknown[]
-	: Obj[keyof Obj][];
+declare function values<T extends any[]>(obj: T): T;
+declare function values<T extends String>(obj: T): string[];
+declare function values<T extends Number|Boolean>(obj: T): never[];
+declare function values<T extends Record<string, any>>(obj: T): T[keyof T][];
 
 export default values
