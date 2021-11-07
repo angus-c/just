36a28cab62049fb510f056fb14d69128a4c72adb
Max Ryan Synnott <maxryansynnott@gmail.com>
2021-11-07 19:33:19 +0800

    Added types for just-prune

--- packages/string-truncate/index.js
+++ packages/string-truncate/index.js
@@ -3,7 +3,7 @@ module.exports = truncate;
 /*
   truncate('when shall we three meet again', 9); // 'when s...'
   truncate('when shall we three meet again', 10, ' (etc)'); // 'when (etc)'
-  truncate('when shall we', 15,); // 'when shall we'
+  truncate('when shall we', 15); // 'when shall we'
   truncate('when shall we', 15, '(more)'); // 'when shall we'
   truncate('when shall we', 7, ' (more)'); // ' (more)'
 */
