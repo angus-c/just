0dc9cc0b70b0279dbdd1aa42761cd7f75306263c
Farhan Kathawala <farhan.kathawala@gmail.com>
2021-11-06 22:28:22 -0700

    Types: better generic type for just-union (#333)
    Co-authored-by: Farhan Kathawala <farhank@stanford.edu>
    Co-authored-by: angus croll <anguscroll@gmail.com>

--- packages/array-union/index.d.ts
+++ packages/array-union/index.d.ts
@@ -1,1 +1,8 @@
-export default function union<T, U>(arr1: T[], arr2: U[]): (T | U)[]
+export default function union<T extends any[], U extends any[]>(
+  arr1: T,
+  arr2: U
+): [...T, ...U];
+export default function union<
+  T extends readonly any[],
+  U extends readonly any[]
+>(arr1: T, arr2: U): [...T, ...U];
