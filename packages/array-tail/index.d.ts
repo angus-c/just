5af786ab1c275af42217b51ff136cf6d516ebbd2
Farhan Kathawala <farhank@stanford.edu>
2021-11-11 15:11:35 -0800

    Add readonly array handling for array-tail

--- packages/array-tail/index.d.ts
+++ packages/array-tail/index.d.ts
@@ -1,4 +1,4 @@
-export default function tail<Tail>(arr: [unknown, ...Tail[]]): Tail[]
-export default function tail<T>(arr: T[]): T[]
-
-export default function tail<T>(arr: [unknown, ...T[]] | T[]): T[]
+export default function tail<Tail>(arr: [unknown, ...Tail[]]): Tail[];
+export default function tail<T>(arr: T[]): T[];
+export default function tail<T extends any[]>(arr: readonly [any, ...T]): T;
+export default function tail<T>(arr: [unknown, ...T[]] | T[]): T[];
