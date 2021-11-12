20ef7b2ae09a5c5fc657108030c0678f37b54a6e
Farhan Kathawala <farhan.kathawala@gmail.com>
2021-11-11 21:47:24 -0800

    Add readonly array handling for array-tail (#373)
    Co-authored-by: Farhan Kathawala <farhank@stanford.edu>

--- packages/array-tail/index.tests.ts
+++ packages/array-tail/index.tests.ts
@@ -6,6 +6,7 @@ const test2: string[] = tail([1, 'a', 'b']);
 const test3: (number | string)[] = tail([1, 2, 'a', 'b']);
 const test4: boolean[] = tail([true]); // []
 const test5: number[] = tail([] as number[]); // []
+const test6: [2, 3] = tail([1, 2, 3] as const);
 
 // Not OK
 // @ts-expect-error
@@ -14,3 +15,5 @@ tail(); // throws
 tail({}); // throws
 // @ts-expect-error
 tail('array'); // throws
+// @ts-expect-error
+const test7: [1, 2] = tail([1, 2, 3] as const);
