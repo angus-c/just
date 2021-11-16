c4c3ae59ef49580b7dfa0788cd9d2c3d61398558
Max Synnott <maxryansynnott@gmail.com>
2021-11-16 15:46:44 +0800

    Added throws for unexpected arguments and added test cases

--- test/object-deep-map-values/index.js
+++ test/object-deep-map-values/index.js
@@ -6,22 +6,46 @@ const concatFn = (value, key) => key + value;
 
 test('correctly maps using value argument', (t) => {
   t.plan(2);
-  const input1 = {a: 1, b: 2, c: 3};
-  const return1 = deepMapValues(input1, squareFn);
+  const object1 = {a: 1, b: 2, c: 3};
+  const return1 = deepMapValues(object1, squareFn);
   t.deepEqual(return1, {a: 1, b: 4, c: 9});
-  const input2 = {a: 1, b: {c: 2, d: {e: 3}}};
-  const return2 = deepMapValues(input2, squareFn);
+  const object2 = {a: 1, b: {c: 2, d: {e: 3}}};
+  const return2 = deepMapValues(object2, squareFn);
   t.deepEqual(return2, {a: 1, b: {c: 4, d: {e: 9}}});
   t.end();
 });
 
 test('correctly maps using value and key arguments', (t) => {
   t.plan(2);
-  const input1 = {a: 'pple', b: 'anana', c: 'herry'};
-  const return1 = deepMapValues(input1, concatFn);
+  const object1 = {a: 'pple', b: 'anana', c: 'herry'};
+  const return1 = deepMapValues(object1, concatFn);
   t.deepEqual(return1, {a: 'apple', b: 'banana', c: 'cherry'});
-  const input2 = {a: 'pple', b: {c: 'herry', d: {e: 'lderberry'}}};
-  const return2 = deepMapValues(input2, concatFn);
-  t.deepEqual(return2, {a: 'apple', b: {c: 'cherry', d: {e: 'elderberry'}}});
+  const object2 = {a: 'pple', b: {c: 'herry', d: {e: 'lderberry'}}};
+  const return2 = deepMapValues(object2, concatFn);
+  t.deepEqual(return2, {
+    a: 'apple',
+    b: {c: 'cherry', d: {e: 'elderberry'}},
+  });
+  t.end();
+});
+
+test('throws error when provided with invalid arguments', (t) => {
+  t.plan(4);
+  t.throws(
+    () => deepMapValues(),
+    new RegExp('First argument must be an object')
+  );
+  t.throws(
+    () => deepMapValues({}),
+    new RegExp('Second argument must be a function')
+  );
+  t.throws(
+    () => deepMapValues([1, 2, 3], squareFn),
+    new RegExp('First argument must be an object')
+  );
+  t.throws(
+    () => deepMapValues({a: 1, b: 2, c: 3}, {}),
+    new RegExp('Second argument must be a function')
+  );
   t.end();
 });
