61c694057fb1e9410dbb3ab968c2ffdc1bc8ad26
angus-c <anguscroll@gmail.com>
2021-12-29 15:22:51 -0800

    remove comments

--- packages/number-random-integer/index.mjs
+++ packages/number-random-integer/index.mjs
@@ -1,28 +1,5 @@
 var numberRandomInteger = random;
 
-// function random(lower, upper) {
-//   if (lower !== undefined && typeof lower !== 'number') {
-//     throw new Error('lower should be a number');
-//   }
-
-//   if (upper !== undefined && typeof upper !== 'number') {
-//     throw new Error('upper should be a number');
-//   }
-
-//   var from = (typeof lower === 'number' ? lower : 1) + 1;
-//   var end = upper;
-//   var floor = Math.floor;
-//   var random = Math.random;
-
-//   if (end === undefined) {
-//     return floor(random() * (from));
-//   }
-
-//   return floor(
-//     (random() * (end - from)) + from
-//   );
-// }
-
 function random(n1, n2) {
   if (typeof n1 == 'undefined') {
     n1 = 1;
