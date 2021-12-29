a045043a388cf4b5139a8d2444dad59ae30e8a5b
angus-c <anguscroll@gmail.com>
2021-12-29 14:15:10 -0800

    wip

--- packages/number-random-integer/index.js
+++ packages/number-random-integer/index.js
@@ -1,24 +1,48 @@
 module.exports = random;
 
-function random(lower, upper) {
-  if (lower !== undefined && typeof lower !== 'number') {
-    throw new Error('lower should be a number');
+// function random(lower, upper) {
+//   if (lower !== undefined && typeof lower !== 'number') {
+//     throw new Error('lower should be a number');
+//   }
+
+//   if (upper !== undefined && typeof upper !== 'number') {
+//     throw new Error('upper should be a number');
+//   }
+
+//   var from = (typeof lower === 'number' ? lower : 1) + 1;
+//   var end = upper;
+//   var floor = Math.floor;
+//   var random = Math.random;
+
+//   if (end === undefined) {
+//     return floor(random() * (from));
+//   }
+
+//   return floor(
+//     (random() * (end - from)) + from
+//   );
+// }
+
+function random(n1, n2) {
+  if (typeof n1 == 'undefined') {
+    n1 = 1;
+  }
+  if (typeof n2 == 'undefined') {
+    n2 = 0;
+  }
+  if (typeof n1 != 'number' || typeof n2 !== 'number') {
+    throw new Error('just-random: arguments must be numbers');
+  }
+  if (n1 > n2) {
+    let temp = n1;
+    n1 = n2;
+    n2 = temp;
   }
 
-  if (upper !== undefined && typeof upper !== 'number') {
-    throw new Error('upper should be a number');
-  }
+  console.log('******', n1, n2);
+  return ~~(Math.random() * (trunc(n2) + 1 - trunc(n1))) + trunc(n1);
+}
 
-  var from = (typeof lower === 'number' ? lower : 1) + 1;
-  var end = upper;
-  var floor = Math.floor;
-  var random = Math.random;
-
-  if (end === undefined) {
-    return floor(random() * (from));
-  }
-
-  return floor(
-    (random() * (end - from)) + from
-  );
+function trunc(n) {
+  return Math.trunc ? Math.trunc(n) : Math.floor(n) + (n < 0 ? 1 : 0);
 }
--- test/number-random-integer/index.js
+++ test/number-random-integer/index.js
@@ -1,54 +1,116 @@
 var test = require('../util/test')(__filename);
 var random = require('../../packages/number-random-integer');
 
-function expected(value, start, end) {
-  return value >= start && value <= end;
-}
+test.only('returns correct value for a given Math.random() result', function(t) {
+  t.plan(41);
+  var nativeRandomFn = Math.random();
+  function mockRandom(n) {
+    Math.random = function() {
+      return n;
+    };
+  }
 
-test('returns a number between 0 and 1', function(t) {
-  t.plan(5);
+  try {
+    // binary
+    mockRandom(0);
+    t.equal(random(0, 1), 0);
+    mockRandom(0.49999);
+    t.equal(random(0, 1), 0);
+    mockRandom(0.5);
+    t.equal(random(0, 1), 1);
+    mockRandom(0.51);
+    t.equal(random(0, 1), 1);
 
-  t.ok(expected(random(), 0, 1));
-  t.ok(expected(random(), 0, 1));
-  t.ok(expected(random(), 0, 1));
-  t.ok(expected(random(), 0, 1));
-  t.ok(expected(random(), 0, 1));
+    // bigger range
+    mockRandom(0);
+    t.equal(random(0, 19), 0);
+    mockRandom(0.0499999);
+    t.equal(random(0, 19), 0);
+    mockRandom(0.05);
+    t.equal(random(0, 19), 1);
+    mockRandom(0.25);
+    t.equal(random(0, 19), 5);
+    mockRandom(0.40);
+    t.equal(random(0, 19), 8);
+    mockRandom(0.499999);
+    t.equal(random(0, 19), 9);
+    mockRandom(0.5);
+    t.equal(random(0, 19), 10);
+    mockRandom(0.94);
+    t.equal(random(0, 19), 18);
+    mockRandom(0.95);
+    t.equal(random(0, 19), 19);
+    mockRandom(0.999999);
+    t.equal(random(0, 19), 19);
 
-  t.end();
-});
+    // non integer range
+    mockRandom(0);
+    t.equal(random(-0.7, 19.3), 0);
+    mockRandom(0.999999);
+    t.equal(random(-0.7, 19.3), 19);
 
-test('returns a number between 5 and 10', function(t) {
-  t.plan(5);
+    // negative to positive range
+    mockRandom(0);
+    t.equal(random(-6, 7), -6);
+    mockRandom(0.4999999);
+    t.equal(random(-6, 7), 0);
+    mockRandom(0.5);
+    t.equal(random(-6, 7), 1);
+    mockRandom(0.99999);
+    t.equal(random(-6, 7), 7);
 
-  t.ok(expected(random(5, 10), 5, 10));
-  t.ok(expected(random(5, 10), 5, 10));
-  t.ok(expected(random(5, 10), 5, 10));
-  t.ok(expected(random(5, 10), 5, 10));
-  t.ok(expected(random(5, 10), 5, 10));
+    // negative binary
+    mockRandom(0);
+    t.equal(random(-1, 0), -1);
+    mockRandom(0.4999999);
+    t.equal(random(-1, 0), -1);
+    mockRandom(0.5);
+    t.equal(random(-1, 0), 0);
+    mockRandom(0.99999);
+    t.equal(random(-1, 0), 0);
 
-  t.end();
-});
+    // negative to negative range
+    mockRandom(0);
+    t.equal(random(-11, -3), -11);
+    mockRandom(0.44);
+    t.equal(random(-11, -3), -8);
+    mockRandom(0.45);
+    t.equal(random(-11, -3), -7);
+    mockRandom(0.5);
+    t.equal(random(-11, -3), -7);
+    mockRandom(0.99999);
+    t.equal(random(-11, -3), -3);
 
-test('returns a number from 0 to 5', function(t) {
-  t.plan(5);
+    // no range
+    mockRandom(0);
+    t.equal(random(0), 0);
+    mockRandom(0.99999);
+    t.equal(random(0), 0);
+    mockRandom(0);
+    t.equal(random(3, 3), 3);
+    mockRandom(0.5);
+    t.equal(random(3, 3), 3);
+    mockRandom(0.78);
+    t.equal(random(-7, -7), -7);
+    mockRandom(0.5);
+    t.equal(random(0, 0), 0);
+    mockRandom(0.9999);
+    t.equal(random(100, 100), 100);
+    mockRandom(0.34);
+    t.equal(random(-18, -18), -18);
 
-  t.ok(expected(random(5), 0, 5));
-  t.ok(expected(random(5), 0, 5));
-  t.ok(expected(random(5), 0, 5));
-  t.ok(expected(random(5), 0, 5));
-  t.ok(expected(random(5), 0, 5));
-
-  t.end();
-});
-
-test('returns a number between -5 and -10', function(t) {
-  t.plan(5);
-
-  t.ok(expected(random(-5, -10), -10, -5));
-  t.ok(expected(random(-5, -10), -10, -5));
-  t.ok(expected(random(-5, -10), -10, -5));
-  t.ok(expected(random(-5, -10), -10, -5));
-  t.ok(expected(random(-5, -10), -10, -5));
+    // switches args when higher number first
+    mockRandom(0);
+    t.equal(random(15, 4), 4);
+    mockRandom(0.44);
+    t.equal(random(-3, -11), -8);
+    mockRandom(0.499999);
+    t.equal(random(19, 0), 9);
+    mockRandom(0);
+    t.equal(random(7, -6), -6);
+  } catch(e) {} finally {
+    Math.random = nativeRandomFn;
+  }
 
   t.end();
 });
