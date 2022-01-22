d222a1f3a222a1427f3badb33a6a622bbc812e88
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-01-22 19:10:34 -0300

    Improve collectionCompare (#405)
    * implement cancel method to throttle
    
    * remove sort method from the compareObject function

--- packages/collection-compare/index.js
+++ packages/collection-compare/index.js
@@ -18,32 +18,39 @@ function compare(value1, value2) {
   if (value1 === value2) {
     return true;
   }
+
   /* eslint-disable no-self-compare */
   // if both values are NaNs return true
   if (value1 !== value1 && value2 !== value2) {
     return true;
   }
+
   if ({}.toString.call(value1) != {}.toString.call(value2)) {
     return false;
   }
+
   if (value1 !== Object(value1)) {
     // non equal primitives
     return false;
   }
+
   if (!value1) {
     return false;
   }
+
   if (Array.isArray(value1)) {
     return compareArrays(value1, value2);
   }
+
   if ({}.toString.call(value1) == '[object Set]') {
     return compareArrays(Array.from(value1), Array.from(value2));
   }
+
   if ({}.toString.call(value1) == '[object Object]') {
     return compareObjects(value1, value2);
-  } else {
-    return compareNativeSubtypes(value1, value2);
   }
+
+  return compareNativeSubtypes(value1, value2);
 }
 
 function compareNativeSubtypes(value1, value2) {
@@ -53,32 +60,35 @@ function compareNativeSubtypes(value1, value2) {
 
 function compareArrays(value1, value2) {
   var len = value1.length;
+
   if (len != value2.length) {
     return false;
   }
-  var alike = true;
+
   for (var i = 0; i < len; i++) {
     if (!compare(value1[i], value2[i])) {
-      alike = false;
-      break;
-    }
-  }
-  return alike;
-}
-
-function compareObjects(value1, value2) {
-  var keys1 = Object.keys(value1).sort();
-  var keys2 = Object.keys(value2).sort();
-  var len = keys1.length;
-  if (len != keys2.length) {
-    return false;
-  }
-  for (var i = 0; i < len; i++) {
-    var key1 = keys1[i];
-    var key2 = keys2[i];
-    if (!(key1 == key2 && compare(value1[key1], value2[key2]))) {
       return false;
     }
   }
+
+  return true;
+}
+
+function compareObjects(value1, value2) {
+  var keys1 = Object.keys(value1);
+  var len = keys1.length;
+
+  if (len != Object.keys(value2).length) {
+    return false;
+  }
+
+  for (var i = 0; i < len; i++) {
+    var key1 = keys1[i];
+
+    if (!(value2.hasOwnProperty(key1) && compare(value1[key1], value2[key1]))) {
+      return false;
+    }
+  }
+
   return true;
 }
