1bf82add013230677c596f785f7dd9697f4f0ee9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-01-23 05:17:55 -0300

    remove unnecessary condition

--- packages/array-zip/index.js
+++ packages/array-zip/index.js
@@ -13,17 +13,15 @@ module.exports = zip;
 */
 
 function zip() {
-  var result = [];
   var args = Array.prototype.slice.call(arguments);
   var argsLen = args.length;
   var maxLen = 0;
-  var i, j;
 
   if (!argsLen) {
     throw new Error('zip requires at least one argument');
   }
 
-  for (i = 0; i < argsLen; i++) {
+  for (var i = 0; i < argsLen; i++) {
     if (!Array.isArray(args[i])) {
       throw new Error('all arguments must be arrays');
     }
@@ -33,12 +31,11 @@ function zip() {
     }
   }
 
-  for (i = 0; i < maxLen; i++) {
+  var result = [];
+
+  for (var i = 0; i < maxLen; i++) {
     var group = [];
-    for (j = 0; j < argsLen; j++) {
-      if (!Array.isArray(args[j])) {
-        throw new Error('all arguments must be arrays');
-      }
+    for (var j = 0; j < argsLen; j++) {
       group[j] = args[j][i];
     }
     result[i] = group;
