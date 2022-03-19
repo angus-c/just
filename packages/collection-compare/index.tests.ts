d977043d707769119e8187e314d1a3b5322b8238
Justy Robles <80285168+justyrobles@users.noreply.github.com>
2022-03-19 14:30:49 -0400

    add typings for collection-compare (#427)

---
+++ packages/collection-compare/index.tests.ts
@@ -0,0 +1,71 @@
+import compare from "./index";
+
+type Type1 = {
+  a: number;
+  b: number;
+};
+type Type2 = {
+  c: number;
+  d: number;
+  e: number;
+};
+type Type3 = {
+  a: number;
+  b: number;
+  c: number;
+};
+
+const basicArr1 = [1, [2, 3]];
+const basicArr2 = [1, [2, 3], 4];
+
+const obj1: Type1 = { a: 2, b: 3 };
+const obj2: Type2 = { c: 2, d: 3, e: 4 };
+const obj3: Type3 = { a: 2, b: 3, c: 4 };
+
+const funcA = () => true;
+const funcB = () => (funcA() ? "yes" : undefined);
+const funcC = () => (funcA() ? undefined : "yes");
+
+const num1 = 1;
+const num2 = new Number(1);
+
+// OK
+compare(1, 2);
+compare("1", "2");
+compare(false, true);
+compare(BigInt(123), BigInt(456));
+compare(basicArr1, basicArr1);
+compare(basicArr1, basicArr2);
+compare(obj1, obj1);
+compare(obj1, obj3);
+compare(obj3, obj1);
+compare(funcB, funcC);
+compare(obj1, { b: 3, a: 2 });
+
+compare([1, [2, { a: 4 }], 4], [1, [2, { a: 4 }]]);
+compare([1, [2, { a: 4 }], 4], [1, [2, { a: 4 }], 4]);
+compare(NaN, NaN);
+
+// Not okay
+// @ts-expect-error
+compare();
+// @ts-expect-error
+compare(1, "1");
+// @ts-expect-error
+compare(["abc"], "abc");
+// @ts-expect-error
+compare({ a: 1, b: 2 }, [{ a: 1, b: 2 }]);
+// @ts-expect-error
+compare(obj2, obj1);
+// @ts-expect-error
+compare(obj1, obj2);
+// @ts-expect-error
+compare(NaN, "abc");
+// @ts-expect-error
+compare(funcA, funcB);
+// @ts-expect-error
+compare(funcA, true);
+// @ts-expect-error
+compare(num1, num2);
+// @ts-expect-error
+compare(num2, num1);
