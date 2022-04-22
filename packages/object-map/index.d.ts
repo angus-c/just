9b54ed83acf323e9ac6651942452a3ef000b377c
Sunny Zanchi <sunnyz@hey.com>
2022-04-21 22:50:42 -0400

    Use generic for Record key type
    This lets the function pass through more specific type information

--- packages/object-map/index.d.ts
+++ packages/object-map/index.d.ts
@@ -1,6 +1,6 @@
-declare function map<T, U = T>(
-  item: Record<string, T>,
-  callback: (key: string, value: T) => U
-): Record<string, U>;
+declare function map<K extends string | symbol, T, U = T>(
+  item: Record<K, T>,
+  callback: (key: K, value: T) => U,
+): Record<K, U>
 
-export default map;
+export default map
--- packages/object-map/index.test.ts
+++ packages/object-map/index.test.ts
@@ -13,10 +13,16 @@ const test3: Record<string, string> = map(
   { a: "pple" },
   (key, value) => key + value
 );
+type MyObject = {
+  k1: number,
+  k2: number,
+}
+const myObject: MyObject = { k1: 1, k2: 2 };
+const test4: MyObject = map(myObject, (key: keyof MyObject, value) => value + 1);
 
 // Not OK
 // @ts-expect-error
-const test4: Record<string, string> = map({ foo: 1 }, (_, value) => value + 1);
+const test5: Record<string, string> = map({ foo: 1 }, (_, value) => value + 1);
 // @ts-expect-error
 map();
 // @ts-expect-error
