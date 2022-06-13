760c68b9bbdffea8dfbe4aec47ea96f8b9e075d5
alexandredev3 <costasalexandre511@gmail.com>
2022-06-13 11:39:13 +0000

    Add generic for object-merge type

--- packages/object-merge/index.d.ts
+++ packages/object-merge/index.d.ts
@@ -1,3 +1,6 @@
 // Definitions by: nokazn <https://github.com/nokazn>
-declare function merge(obj1: object, ...objs: object[]): object;
+declare function merge<
+  TObj1 extends object = object,
+  TObjs extends object = object
+>(obj1: TObj1, ...objs: TObjs[]): TObj1 & TObjs;
 export default merge;
