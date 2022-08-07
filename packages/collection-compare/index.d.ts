ee4d7cdc5175bcfff41d81fb8d4250943a48585e
Neil Morrison <neil.morrison44@googlemail.com>
2022-08-07 23:48:25 +0100

    Simplifies typings on `just-compare` (#480)
    * Simplifies typings on `just-compare`
    
    * remove all the TS tests which are now fine

--- packages/collection-compare/index.d.ts
+++ packages/collection-compare/index.d.ts
@@ -1,17 +1,3 @@
-// Definitions by: Justy Robles <https://github.com/justyrobles>
-
-type Primitive = boolean | string | number | bigint | null | undefined;
-
-declare function compare<T extends Primitive>(value1: T, value2: T): boolean;
-
-declare function compare<T1 extends object, T2 extends object & T1>(
-  value1: T1,
-  value2: T2
-): boolean;
-
-declare function compare<T1 extends object & T2, T2 extends object>(
-  value1: T1,
-  value2: T2
-): boolean;
+declare function compare(value1: unknown, value2: unknown): boolean;
 
 export default compare;
